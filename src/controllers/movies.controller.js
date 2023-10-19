517d52fb74e04b4474ebc88fa619033c9fa31f7d
EdwanNidzar <56621669+EdwanNidzar@users.noreply.github.com>
2023-10-19 23:48:53 +0800

    feat : func upload file

--- src/controllers/movies.controller.js
+++ src/controllers/movies.controller.js
@@ -1,5 +1,22 @@
 const movieService = require("../services/movies.service");
 
+const multer = require("multer");
+const path = require("path");
+
+const diskStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../../public/uploads"));
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({ storage: diskStorage });
+
 const getAllMovies = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
@@ -14,12 +31,33 @@ const getAllMovies = async (req, res) => {
 
 const createMovie = async (req, res) => {
   try {
-    const movieData = req.body;
-    const movie = await movieService.createMovie(movieData);
+    upload.single("photo")(req, res, async (err) => {
+      if (err) {
+        return res.status(400).send("Error uploading file");
+      }
 
-    res.send({
-      data: movie,
-      message: "create movie success",
+      const file = req.file;
+      const movieData = req.body;
+
+      const requiredFields = ["title", "genres", "year"];
+
+      for (const field of requiredFields) {
+        if (!movieData[field]) {
+          return res.status(400).send(`Field ${field} is required`);
+        }
+      }
+
+      const photoPath = file.path;
+
+      const movie = await movieService.createMovie({
+        ...movieData,
+        photo: photoPath,
+      });
+
+      res.send({
+        data: movie,
+        message: "create movie success",
+      });
     });
   } catch (error) {
     res.status(400).send(error.message);
@@ -38,12 +76,30 @@ const getMovieById = async (req, res) => {
 
 const editMovie = async (req, res) => {
   try {
-    const movieId = parseInt(req.params.id);
-    const movieData = req.body;
+    upload.single("photo")(req, res, async (err) => {
+      if (err) {
+        return res.status(400).send("Error uploading file");
+      }
 
-    const movie = await movieService.editMovie(movieId, movieData);
+      const movieId = parseInt(req.params.id);
+      const movieData = req.body;
 
-    res.json({ data: movie, message: "edit movie success" });
+      const requiredFields = ["title", "genres", "year"];
+
+      for (const field of requiredFields) {
+        if (!movieData[field]) {
+          return res.status(400).send(`Field '${field}' is required`);
+        }
+      }
+
+      if (req.file) {
+        movieData.photo = req.file.path;
+      }
+
+      const movie = await movieService.editMovie(movieId, movieData);
+
+      res.json({ data: movie, message: "edit movie success" });
+    });
   } catch (error) {
     res.status(400).send(error.message);
   }
